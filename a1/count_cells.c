f3d586e186cdf4c2c355c1b78a97d342c6871357
areej <areejnegm@gmail.com>
2019-10-02 13:21:37 -0400

    Finished A1

--- a1/count_cells.c
+++ a1/count_cells.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void read_image(int num_rows, int num_cols, 
                 int arr[num_rows][num_cols], FILE *fp);
                 
 void print_image(int num_rows, int num_cols, int arr[num_rows][num_cols]);
 
-// Remember to include the function prototype for count_cells
-
+int count_cells(int num_rows, int num_cols, int arr[num_rows][num_cols]);
 
 int main(int argc, char **argv) {
-    // Print a message to stderr and exit with an argument of 1 if there are
-    // not the right number of parameters, or the second argument is not -p
-
-
+	// > count_cells ./img.txt
+	// ----> argc is 2, argv is ["count_cells", "./img.txt"]
+	// > count_cells ./img.txt -p
+	// ----> argc is 3, argv is ["count_cells", "./img.txt", "-p"]
+	if (argc <= 1 || argc >= 4 || (argc == 3 && strcmp(argv[2], "-p") != 0)) {
+		fprintf(stderr, "Usage: count_cells <imagefile.txt> [-p]\n");
+		return 1;
+	}
+	FILE *fp;
+	fp = fopen(argv[1], "r");
+	int num_rows, num_cols;
+	fscanf(fp, "%d %d", &num_rows, &num_cols);
+	int arr[num_rows][num_cols];
+	read_image(num_rows, num_cols, arr, fp);
+	fclose(fp);
+	if (argc == 3) print_image(num_rows, num_cols, arr);
+	printf("Number of Cells is %d\n", count_cells(num_rows, num_cols, arr));
     return 0;
 }
--- a1/image.c
+++ a1/image.c
@@ -5,12 +5,40 @@
  */
 void read_image(int num_rows, int num_cols, 
                 int arr[num_rows][num_cols], FILE *fp) {
-
+	for (int y = 0; y < num_rows; y++) {
+		for (int x = 0; x < num_cols; x++) {
+			if (!fscanf(fp, "%d", &arr[y][x])) arr[y][x] = 0;
+		}
+	}
 }
 
 /* Print to standard output the contents of the array arr */
 void print_image(int num_rows, int num_cols, int arr[num_rows][num_cols]) {
-
+	for (int y = 0; y < num_rows; y++) {
+		for (int x = 0; x < num_cols - 1; x++) {
+			printf("%d ", arr[y][x]);
+		}
+		printf("%d\n", arr[y][num_cols - 1]);
+	}
 }
 
-/* TODO: Write the count_cells function */
+void mark_cell(int num_rows, int num_cols, int arr[num_rows][num_cols], int y, int x) {
+	arr[y][x] = 256;
+	if (y > 0 && arr[y - 1][x] == 255) mark_cell(num_rows, num_cols, arr, y - 1, x);
+	if (y < num_rows - 1 && arr[y + 1][x] == 255) mark_cell(num_rows, num_cols, arr, y + 1, x);
+	if (x > 0 && arr[y][x - 1] == 255) mark_cell(num_rows, num_cols, arr, y, x - 1);
+	if (x < num_cols - 1 && arr[y][x + 1] == 255) mark_cell(num_rows, num_cols, arr, y, x + 1);
+}
+
+int count_cells(int num_rows, int num_cols, int arr[num_rows][num_cols]) {
+	int count = 0;
+	for (int y = 0; y < num_rows; y++) {
+		for (int x = 0; x < num_cols; x++) {
+			if (arr[y][x] == 255) {
+				mark_cell(num_rows, num_cols, arr, y, x);
+				count++;
+			}
+		}
+	}
+	return count;
+}
